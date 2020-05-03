328ffda2e00f9e4b3b9b129fa50c918708de0c87
James Berg <james.berg38@yahoo.com>
2020-05-03 12:32:11 -0500

    create Nav.js skeloton and installed react-router-dom

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function Nav(){
+
+    const location = useLocation();
+
+    return(
+        <div>
+            Nav.js
+        </div>
+    );
+
+}
+
+export default Nav
