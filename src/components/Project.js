0e67bb10f73a76ca9b6e9b4e5133c6505e50bf8e
James Berg <james.berg38@yahoo.com>
2020-05-05 09:34:59 -0500

    created basic bootstrap container structure

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,12 +1,38 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Project(){
 
     return(
 
-        <div>
-            Project.js
-        </div>
+        <Container>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+        </Container>
 
     );
 
