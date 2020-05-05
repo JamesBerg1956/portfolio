9719066e1790d23b288243f0229aed70ec6f9fcc
James Berg <james.berg38@yahoo.com>
2020-05-05 09:45:47 -0500

    fixed overwriting this file

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,38 @@
 import React from "react";
-import Project from "../Project.js"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Project(){
 
     return(
 
-        <Project />
+        <Container>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} lg={6}>
+
+                </Col>
+                <Col md={12} lg={6}>
+                    
+                </Col>
+            </Row>
+        </Container>
 
     );
 
